4dddf5cd9fcab1353f3e53e2b305d3432ac0951e
Jussi Mullo <mulloju@gmail.com>
2020-11-06 00:36:07 +0200

    Formatting

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -8,13 +8,16 @@ export const Footer = () => {
     return (
         <div className="footer">
             <div className="link">
-                <FontAwesomeIcon className="icon" icon={faEnvelope} /><a href="mailto:jussi.mullo@iki.fi">jussi.mullo@iki.fi</a>
+                <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                <a href="mailto:jussi.mullo@iki.fi">jussi.mullo@iki.fi</a>
             </div>
             <div className="link">
-                <FontAwesomeIcon className="icon" icon={faGithub} /><a href="https://github.com/jmullo/touch-type">GitHub</a>
+                <FontAwesomeIcon className="icon" icon={faGithub} />
+                <a href="https://github.com/jmullo/touch-type">GitHub</a>
             </div>
             <div className="link">
-                <FontAwesomeIcon className="icon" icon={faDonate} /><a href="https://www.paypal.me/jmullo">Donate with PayPal</a>
+                <FontAwesomeIcon className="icon" icon={faDonate} />
+                <a href="https://www.paypal.me/jmullo">Donate with PayPal</a>
             </div>
         </div>
     );
