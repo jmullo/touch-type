821f35e9efd929901f7aab9073eda45dd4f382de
Jussi Mullo <mulloju@gmail.com>
2020-11-17 22:34:19 +0200

    More words

--- src/data/clean.sh
+++ src/data/clean.sh
@@ -2,7 +2,17 @@
 sed -r 's/\s+//g' -i $1
 
 # remove words containing numbers
-sed '/[0-9]/d' -i $1
+sed '/[[:digit:]]/d' -i $1
+
+# remove punctuation characters except hyphen
+sed '/[[:punct:]]*/{s/[^[:alpha:]-]//g}' -i $1
+
+# remove multiple hyphens
+sed -r '\-^([^\-]*\-[^\-]*){2,10}$-d' -i $1
+
+# remove leading and trailing hyphens
+sed '/^\-/d' -i $1
+sed '/\-$/d' -i $1
 
 # remove words shorter than 3 characters
 sed -r '/^.{,2}$/d' -i $1
